226de5fcbbf4f4f953347490d69805935dff639a
Muyang <shimy17@163.com>
2019-06-25 09:47:01 -0500

    flask app.py run on localhost

--- app.py
+++ app.py
@@ -5,11 +5,34 @@
 
 import sys
 import json
+import random
 import flask
-import config
+# import config
 # import psycopg2
 
 app = flask.Flask(__name__)
+
+# def get_connection():
+# 	'''
+# 	Returns a connection to the database described
+# 	in the config module. Returns None if the
+# 	connection attempt fails.
+# 	'''
+# 	connection = None
+# 	try:
+# 		connection = psycopg2.connect(database=config.database,
+# 									  user=config.user,
+# 									  password=config.password)
+# 	except:
+# 		try:
+# 			connection = psycopg2.connect(database=config_johnny.database,
+# 									  user=config_johnny.user,
+# 									  password=config_johnny.password)
+# 		except Exception as e:
+# 			print(e, file=sys.stderr)
+# 	return connection
+
+words = ['apple','orange','pear','peach','tangerine','banana']
 
 @app.after_request
 def set_headers(response):
@@ -20,9 +43,14 @@ def set_headers(response):
 def hello():
 	return 'Hello, Tuckers.'
 
-@app.route('/randomStim/<num words>')
-def get_random_cloud():
-    pass
+@app.route('/randomStim/<numberOfWords>')
+def get_random_cloud(numberOfWords):
+    try:
+        random_words = random.sample(words, int(numberOfWords))
+    except ValueError:
+        print('numberOfWords is too big!')
+    return json.dumps(random_words)
+
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
