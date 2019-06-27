04df38fa055918400a67ef75319dfc2b0dac265c
Muyang <shimy17@163.com>
2019-06-27 17:43:25 -0500

    Append in the csvfile

--- app.py
+++ app.py
@@ -59,26 +59,36 @@ def randomStim(numberOfWords):
         return json.dumps(random_words)
     except ValueError:
         print('numberOfWords is too big!')
-        
-@app.route('/randomStim/receive_data', methods = ['POST'])
-def receive_data():
-    data = flask.request.form.to_dict()
+
+span_tags =''
+
+@app.route('/randomStim/post_data', methods = ['POST'])
+def post_data():
+    data = flask.request.form
     clickedword = data["word"]
     word_x = data["word position[left]"]
     word_y = data["word position[top]"]
     cloud_width = data["container size[width]"]
     cloud_height = data["container size[height]"]
     num_of_Stim = data["number of Stim"]
-    cloud_info = data["cloud"]
+    span_tags = data["cloud"]
 
-    print('clicked word: ' + clickedword, word_x, word_y)
-    print('cloud information: ' + num_of_Stim, cloud_info)
-    with open('client_data.csv','w', newline='') as csvfile:
-        # fieldnames = ['clickedword','word_x','word_y','cloud_width','cloud_height','num_of_Stim','cloud_info']
-        writer = csv.writer(csvfile, delimiter = ',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([clickedword, word_x, word_y, cloud_width, cloud_height, num_of_Stim, cloud_info])
+    # print('clicked word: ' + clickedword, word_x, word_y)
+    # print('cloud information: ' + span_tags)
+    with open('client_data.csv','a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter = ',', quotechar='"')
+        writer.writerow([clickedword, word_x, word_y, cloud_width, cloud_height, num_of_Stim, span_tags])
+
+    # with open('client_data.csv',newline='') as csvfile:
+    #     text = csv.reader(csvfile,delimiter = ',', quotechar='"')
+    #     for row in text:
+    #         print(','.join(row))
     return json.dumps(data)
 
+# @app.route('/received_data/<>')
+# def something(the_cloud):
+#     the_cloud = span_tags
+#     return flask.render_template('receive_data.html', cloud_info=the_cloud)
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
