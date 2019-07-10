69f792d84f762ed96e30dd1973216985795aa39c
Muyang <shimy17@163.com>
2019-07-10 15:24:33 -0500

    Create config.csv and .py that reads it

---
+++ Configures/test_length_config.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+word = 'pseudoword'
+numberOfWords = 40
+minSize = 10
+maxSize = 30
+minLen = 3
+maxLen = 10
+
+def loadTask(tasklist_csv_path):
+    tasklist = []
+    with open(tasklist_csv_path, 'r',newline='') as configFile:
+        reader = csv.reader(configFile,delimiter = ',')
+        heading = next(reader) # Do not use reader.next() because that is python 2
+        for row in reader:
+            this_task = {}
+            for i in range(len(heading)):
+                this_task[heading[i]] = int(row[i])
+            tasklist.append(this_task)
+    print(tasklist)
+    return tasklist
+
+tasklist_csv = os.path.join(os.path.dirname(os.path.abspath(__file__)),'test_length.csv')
+# print(tasklist_csv)
+tasklist = loadTask(tasklist_csv)
