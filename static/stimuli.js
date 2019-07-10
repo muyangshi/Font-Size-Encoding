8de198818576a06e2e0215ea2c52301d37e4463c
Muyang <shimy17@163.com>
2019-07-10 15:27:05 -0500

    tasklist is now more info
    tasklist changed, tasklist[0] being the current "this_task", which is consist of t1fs,t1l,t2fs,t2l

--- static/stimuli.js
+++ static/stimuli.js
@@ -18,15 +18,31 @@ if (window.devicePixelRatio != 1){
 
 
 var words;
-var task_list = read_list; // to be passed from the server, which read a csv
-console.log(task_list);
+console.log(tasklist,tasklist[0]);
+// console.log(read_list);
+// var task_list = read_list; // to be passed from the server, which read a csv
+// console.log(task_list);
 
 
 function onStartButtonClicked() {
-    var num = task_list[0]; //somthing to be read from an Array, that is from the CSV
+    var task = tasklist[0]; //somthing to be read from an Array, that is from the CSV
+    // var target_1_fontsize = this_task["target_1_fontsize"];
 
+    var this_task = {
+        "target_1_fontsize": task["target_1_fontsize"],
+        "target_1_length": task["target_1_length"],
+        "target_2_fontsize": task["target_2_fontsize"],
+        "target_2_length": task["target_2_length"],
+    };
+    console.log(this_task)
+
+    
     $.ajax({
-        url: flask_util.url_for('getStim', {numberOfWords: num}),
+        url: flask_util.url_for('getStim', {
+            target_1_fontsize: task["target_1_fontsize"],
+            target_1_length: task["target_1_length"],
+            target_2_fontsize: task["target_2_fontsize"],
+            target_2_length: task["target_2_length"]}),
         // data: data,
         success:
             function(data){
@@ -37,7 +53,8 @@ function onStartButtonClicked() {
                         weight: dictionary['fontsize'],
                         html: {class: dictionary['html']},
                         handlers: { 
-                            click: function() {postData($(this));} 
+                            click: function() {postData($(this));},
+                            mouseover: function() {this.style.cursor = 'pointer';} 
                         }
                     }
                 });
@@ -62,19 +79,34 @@ function createCloud() {
 }
 
 
-function postData(theWord){
+function postData(clickedWord){
     var container = document.getElementById("JQWC");
     var containerSize = $(".jqcloud").css(['width','height']);
     var numberOfCloudWords = container.childElementCount;
     var theCloud = container.innerHTML;
     
-    var wordPosition = theWord.css(["left","top"]);
-        
+    var clickedWordPosition = clickedWord.css(["left","top"]);
+
+    var targets_left = []
+    var targets_top = []
+    $('.target').each(function(){targets_left.push(parseInt($(this).css('left'),10));});
+    $('.target').each(function(){targets_top.push(parseInt($(this).css('left'),10));});
+    var targets_x_distiance = Math.abs(targets_left[0] - targets_left[1]);
+    var targets_y_distance = Math.abs(targets_top[0] - targets_top[1]);
+
+    var targets_distance = Math.sqrt(Math.pow(targets_x_distiance,2)+Math.pow(targets_y_distance,2))
+
+    var targets_word = []
+    $('.target').each(function(){targets_word.push($(this));})
+    var correct_word = (parseInt(targets_word[0].css('font-size')) > parseInt(targets_word[1].css('font-size'))) ? targets_word[0]:targets_word[1]
+
     var word_data = {
         "turker_id": turker_id,
         "container size": containerSize,
-        "word": theWord[0].innerHTML, //the word itself, but why $(this)[0]
-        "word position": wordPosition,
+        "word": clickedWord[0].innerHTML,
+        "word position": clickedWordPosition,
+        "targets distance": targets_distance,
+        "correct word": correct_word[0].innerHTML,
         "number of Stim": numberOfCloudWords,
         "cloud": theCloud,
     };
@@ -99,8 +131,8 @@ function postData(theWord){
 
 function nextTask(){
     document.getElementById('JQWC').innerHTML = "";
-    task_list.shift();
-    if (task_list.length == 0){
+    tasklist.shift();
+    if (tasklist.length == 0){
         console.log('All tasks completed')
         var input = $("<input>").attr("type","hidden").attr("name","turker_id").val(turker_id);
         $('#get_completion_page').append(input).submit();
