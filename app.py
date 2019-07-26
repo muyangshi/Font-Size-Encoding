edafb7e2b32168691eb8f6f63a14db40001bad6e
Muyang <shimy17@163.com>
2019-07-26 14:41:32 -0500

    minor edit

--- app.py
+++ app.py
@@ -274,12 +274,7 @@ def post_data():
         # 'correct_word_x','correct_word_y','correct_word_fontsize','correct_word_width','correct_word_height','correct_word_center_distance',
         # 'wrong_word_x','wrong_word_y','wrong_word_fontsize','wrong_word_width','wrong_word_height','wrong_word_center_distance',
         # 'number_of_words','span_content'])
-        writer.writerow([turker_id,
-        cloud_width,cloud_height,cloud_center_x,cloud_center_y,
-        clicked_word,correct_word,wrong_word,distance_between_targets,
-        correct_word_x,correct_word_y,correct_word_fontsize,correct_word_width,correct_word_height,correct_word_center_distance,
-        wrong_word_x,wrong_word_y,wrong_word_fontsize,wrong_word_width,wrong_word_height,wrong_word_center_distance,
-        number_of_words,span_content])
+        writer.writerow([turker_id,cloud_width,cloud_height,cloud_center_x,cloud_center_y,clicked_word,correct_word,wrong_word,distance_between_targets,correct_word_x,correct_word_y,correct_word_fontsize,correct_word_width,correct_word_height,correct_word_center_distance,wrong_word_x,wrong_word_y,wrong_word_fontsize,wrong_word_width,wrong_word_height,wrong_word_center_distance,number_of_words,span_content])
     return json.dumps("data")
 
 
