53aad66eebf1e7c66f684576be5adb2a70167831
Muyang <shimy17@163.com>
2019-08-15 09:43:47 -0500

    aug_15 load

--- db_to_csv.py
+++ db_to_csv.py
@@ -14,10 +14,7 @@ def get_connection():
 	'''
 	connection = None
 	try:
-		connection = psycopg2.connect(host='localhost',
-                                      database='fontsize',
-									  user='fontsize',
-									  password='wordcloudsbad?')
+		connection = psycopg2.connect(host='localhost',database='fontsize',user='fontsize',password='wordcloudsbad?')
 	except Exception as e:
 		print(e, file=sys.stderr)
 	return connection
@@ -28,7 +25,7 @@ def get_stimuli_data():
     cursor.execute("SELECT turker_id,clicked_word,correct_word,wrong_word,distance_between_targets,time,correct_word_fontsize,wrong_word_fontsize,correct_word_width,wrong_word_width FROM pilot_opposite_on_circle")
     data_list = cursor.fetchall()
     print(data_list[0])
-    with open('pilot_Aug_13.csv','a',newline='') as csvdata:
+    with open('pilot_Aug_15.csv','a',newline='') as csvdata:
         writer = csv.writer(csvdata,delimiter = ',',quotechar = '"')
         for row in data_list:
             writer.writerow(row)
